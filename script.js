6e2460f0c29d93e258e8bb9ebaa89593f2faeee5
kfusilier <kristinfusilier@gmail.com>
2021-11-30 18:57:49 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function sanityCheck() {
+	console.log("I'm fine!")
+}
+sanityCheck()
+
+//random
+//www.thecocktaildb.com/api/json/v1/1/random.php
+
+// by name
+// https://www.thecocktaildb.com/api/json/v1/1/search.php?s=
+
+// Get Random Drink
+
+async function getRandomDrink(e)	{
+	e.preventDefault()
+	const urlRandom = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+	const response = await fetch(urlRandom)
+	const data = await response.json()
+	console.log(data.drinks[0].strDrink)
+	$("#randomOutput").html(data.drinks[0].strDrink)
+}
+
+$('button').click(getRandomDrink)
+
+// get by ingredient 
+
+async function getIngredient(e) {
+	e.preventDefault()
+	const userInput = $('input[type="text"]').val()
+	const urlIngredient = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userInput}`
+	const response = await fetch(urlIngredient)
+	const data = await response.json()
+	console.log(data.drinks)
+	$('#drink1').html(data.drinks[0].strDrink)
+	$('#drink2').html(data.drinks[1].strDrink)
+	$('#drink3').html(data.drinks[2].strDrink)
+}
+
+$('form').click(getIngredient)
